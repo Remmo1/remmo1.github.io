f7977f7b49314f8ce392bf8fd1d88e6f10b9c56c
Remigiusz Pisarski <remigiusz.pisarski@onet.pl>
2026-07-17 12:19:59 +0200

    Remove typos

--- bizarre-bar/src/data/experience.ts
+++ bizarre-bar/src/data/experience.ts
@@ -12,24 +12,24 @@ export type ExperienceEntry = {
 
 export const experience: ExperienceEntry[] = [
   {
-    company: { pl: 'Convisa', en: 'Convisa' },
+    company: { pl: 'Convista', en: 'Convista' },
     role: { pl: 'Mid Java Developer', en: 'Mid Java Developer' },
     description: {
       pl:
-        'Pracuję nad rozwojem i utrzymaniem mikroserwisów oraz optymalizacją backendu systemów AOK CX.',
+        'Pracuję nad rozwojem i utrzymaniem mikroserwisów oraz optymalizacją backendu systemów niemieckiego klienta.',
       en:
-        'I work on developing and maintaining microservices and optimizing the backend of the AOK CX systems.',
+        'I work on developing and maintaining microservices and optimizing the backend of systems for a German client.',
     },
     period: { pl: 'styczeń 2025 - obecnie', en: 'January 2025 - present' },
   },
   {
-    company: { pl: 'Convisa', en: 'Convisa' },
+    company: { pl: 'Convista', en: 'Convista' },
     role: { pl: 'Java Junior Developer', en: 'Java Junior Developer' },
     description: {
       pl:
-        'Pracuję nad rozwojem i utrzymaniem mikroserwisów oraz optymalizacją backendu systemów AOK CX.',
+        'Pracuję nad rozwojem i utrzymaniem mikroserwisów oraz optymalizacją backendu systemów niemieckiego klienta.',
       en:
-        'I work on developing and maintaining microservices and optimizing the backend of the AOK CX systems.',
+        'I work on developing and maintaining microservices and optimizing the backend of systems for a German client.',
     },
     period: { pl: 'styczeń 2023 - grudzień 2024', en: 'January 2023 - December 2024' },
   },
@@ -38,9 +38,9 @@ export const experience: ExperienceEntry[] = [
     role: { pl: 'Java Trainee', en: 'Java Trainee' },
     description: {
       pl:
-        'Pierwsze doświadczenie w pracy jako stażysta Javy. Staż zakończyłem w grudniu 2022, od stycznia 2023 rozpocząłem pracę przy projekcie AOK CX.',
+        'Pierwsze doświadczenie w pracy jako stażysta Javy. Staż zakończyłem w grudniu 2022, od stycznia 2023 rozpocząłem pracę przy projekcie niemieckiego klienta.',
       en:
-        'My first experience working as a Java intern. I finished the internship in December 2022 and began working on the AOK CX project in January 2023.',
+        'My first experience working as a Java intern. I finished the internship in December 2022 and began working on a project for a German client in January 2023.',
     },
     period: { pl: 'lipiec 2022 - grudzień 2022', en: 'July 2022 - December 2022' },
   }
